420356d1c8cb25da1df77c0bdea98b5fd4a4ce53
FezZHD <fezzdev@gmail.com>
2017-03-08 02:57:35 +0300

    adding viewmodel

--- kMeans/kMeans/ViewModel/WindowViewModel.cs
+++ kMeans/kMeans/ViewModel/WindowViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace kMeans.ViewModel
+﻿using System.Diagnostics;
+
+namespace kMeans.ViewModel
 {
     public class WindowViewModel: BaseViewModel
     {
-        
+        private double height;
+        public double CanvasHeight
+        {
+            get { return height; }
+            set
+            {
+                Debug.WriteLine(value);
+            }
+        }
     }
 }

6c68b4ac914985b6378a81f1f47cf6496a6e7cdf
FezZHD <fezzdev@gmail.com>
2017-03-09 00:05:11 +0300

    field error checking

---
+++ kMeans/kMeans/EnumConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace kMeans
+{
+    public class EnumConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = (bool) value;
+            if (boolValue)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- kMeans/kMeans/ViewModel/WindowViewModel.cs
+++ kMeans/kMeans/ViewModel/WindowViewModel.cs
@@ -1,10 +1,83 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace kMeans.ViewModel
 {
     public class WindowViewModel: BaseViewModel
     {
 
+        public WindowViewModel()
+        {
+            IsAnError = false;
+            ExecuteTask = new Command((() =>
+            {
+                IsAnError = false;
+                if (!ConvertCheckingWrite())
+                {
+                    return;
+                }
+            }));
+        }
+
+
+        private string classCount;
+
+        public string ClassCount
+        {
+            get { return classCount; }
+            set
+            {
+                classCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string pointsCount;
+
+        public string PointsCount
+        {
+            get
+            {
+                return pointsCount;
+            }
+            set
+            {
+                pointsCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string errorString;
+
+        public string ErrorString
+        {
+            get
+            {
+                return errorString;
+            }
+            set
+            {
+                errorString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool isAnError;
+
+        public bool IsAnError
+        {
+            get
+            {
+                return isAnError;
+            }
+            set
+            {
+                isAnError = value;
+                OnPropertyChanged();
+            }
+        }
 
         private Image image;
         public Image ImageSource
@@ -18,6 +91,35 @@ namespace kMeans.ViewModel
                 image = value;
                 OnPropertyChanged();
             }
+        }
+
+
+        public ICommand ExecuteTask { get; private set; }
+
+
+        private uint classCountNum;
+        private uint pointsCountNum;
+
+        private bool ConvertCheckingWrite()
+        {
+            if ((CheckWriteValue(ClassCount, out classCountNum, "Error input at class count field")) &&
+                CheckWriteValue(PointsCount, out pointsCountNum, "Error input at points count field"))
+            {
+                 return true;
+            }
+            return false;
+        }
+
+
+        private bool CheckWriteValue(string fieldValue,out uint fieldNumValue, string errorMessage)
+        {
+            if (!uint.TryParse(fieldValue, out fieldNumValue))
+            {
+                ErrorString = errorMessage;
+                IsAnError = true;
+                return false;
+            }
+            return true;
         }
     }
 }
